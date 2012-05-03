1bc432cc58b82acd19ecfde93c35923a2d7a7b1b
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-05-03 20:57:15 +0900

    fix example/example

--- example/example.rb
+++ example/example.rb
@@ -11,18 +11,18 @@ require_relative '../lib/abstractstack'
     p stack
     p stack.top     #=> 7
     p stack.bottom  #=> 1
-    stack.pop #=> 7
-    stack.pop #=> 1
+    stack.pop       #=> 7
+    stack.pop       #=> 1
     p stack
     p stack.to_a
-    #stack.pop #=> Exception(Stack::UnderFlow)
+    #stack.pop      #=> Exception(UnderFlow)
 
 # * Size limitation
   
     stack = Stack.new 2
     p stack.limit         #=> 2
     stack << 1 << 7
-    #stack << 9          #=> Exception(Stack::Overflow)
+    #stack << 9           #=> Exception(Overflow)
 
 # * You can get your extended Stack class
 
