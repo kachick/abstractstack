ee2c77e5400221717b5fd7778454303c61c34dab
Kenichi Kamiya <kachick1+github_kachick@gmail.com>
2012-05-03 20:18:31 +0900

    Change the APIs

--- lib/abstractstack.rb
+++ lib/abstractstack.rb
@@ -2,128 +2,165 @@
 
 require 'forwardable'
 
+# @abstract - Define start point of index and/or loop operations
+# @example
+#   class LIFOStack < AbstractStack
+#     include Enumerable
+#
+#     alias_method :each, :lifo_each
+#     alias_method :reverse_each, :fifo_each
+#     alias_method :first, :top
+#     alias_method :last, :bottom
+#
+#     def at(pos)
+#       super pos, :top
+#     end
+#   end
 class AbstractStack
+  extend Forwardable
 
-  VERSION = '0.0.1'.freeze
+  VERSION = '0.0.2'.freeze
 
   class InvalidStackOperation < StandardError; end
+  class UnderFlow < InvalidStackOperation; end
+  class OverFlow < InvalidStackOperation; end
+  
+  attr_reader :limit
+ 
+  def initialize(limit=nil)
+    @list = []
 
-  class << self
-
-    alias_method :_new, :new
-    private :_new
-
-    # @return [Class]
-    def new(&block)
-      ::Class.new(self) {
-        class_eval(&block) if block_given?
-      }
+    case limit
+    when nil, 0, false
+      @limit = nil
+    when ->v{v.integer? && (v >= 1)}
+      @limit = limit.to_int
+    else
+      raise TypeError
     end
+  end
 
-    private
+  def_delegators :@list, :length, :size, :empty?
+  def_delegator  :@list, :first, :bottom
+  def_delegator  :@list, :last, :top
+  def_delegator  :@list, :last, :peek
 
-    alias_method :_inherited, :inherited
+  # @param [Object] value
+  # @return [Object] value
+  def push(value)
+    if limit && (limit <= length)
+      raise OverFlow
+    else
+      @list.push value
+      self
+    end
+  end
+
+  alias_method :<<, :push
+
+  # @return [Object]
+  def pop
+    if empty?
+      raise UnderFlow
+    else
+      @list.pop
+    end
+  end
+
+  # @return [self]
+  def fifo_each
+    return to_enum(__callee__) unless block_given?
+
+    @list.each{|v|yield v}
+    self
+  end
+  
+  alias_method :lilo_each, :fifo_each
+
+  # @return [self]
+  def filo_each
+    return to_enum(__callee__) unless block_given?
+
+    @list.reverse_each{|v|yield v}
+    self
+  end
+
+  alias_method :lifo_each, :filo_each
+  
+  # @param [#to_int] pos
+  # @param [Symbol] start
+  # @abstract - Define start point of index
+  def at(pos, start)
+    raise InvalidStackOperation if empty?
     
-    def inherited(subclass)
-      subclass.class_eval do
-
-        _inherited subclass
-        extend Forwardable
-
-        const_set :UnderFlow, Class.new(InvalidStackOperation)
-        const_set :OverFlow,  Class.new(InvalidStackOperation)
-
-        singleton_class.class_eval do
-          alias_method :new, :_new
-          undef_method :_new
-          public :new
-
-          alias_method :inherited, :_inherited
-          undef_method :_inherited
-          private :inherited
-        end
-
-        attr_reader :limit
-       
-        def initialize(limit=nil)
-          @list = []
-
-          case limit
-          when nil, 0, false
-            @limit = nil
-          when ->v{(v >= 1) && v.kind_of?(Integer)}
-            @limit = limit.to_int
-          else
-            raise TypeError
-          end
-        end
-
-        def_delegators :@list, :length, :size, :empty?
-        def_delegator  :@list, :first, :bottom
-        def_delegator  :@list, :last, :top
-        def_delegator  :@list, :last, :peek
-
-        # @param [Object] value
-        # @return [Object] value
-        def push(value)
-          if limit && (limit <= length)
-            raise self.class::OverFlow
-          else
-            @list.push value
-            self
-          end
-        end
-
-        alias_method :<<, :push
- 
-        # @return [Object]
-        def pop
-          if empty?
-            raise self.class::UnderFlow
-          else
-            @list.pop
-          end
-        end
- 
-        # @return [self]
-        def each
-          return to_enum(__callee__) unless block_given?
-
-          @list.each{|v|yield v}
-          self
-        end
-
-        # @return [self]
-        def reverse_each
-          return to_enum(__callee__) unless block_given?
-
-          @list.reverse_each{|v|yield v}
-          self
-        end
-
-        alias_method :lifo_each, :reverse_each
-        alias_method :filo_each, :reverse_each
-
-        # @return [Array]
-        def to_a
-          @list.dup
-        end
-
-        # @return [self]
-        def freeze
-          @list.freeze
-          super
-        end
-
-        private
-
-        def initialize_copy(original)
-          @list = @list.dup
-        end
-        
+    pos = pos.to_int
+    
+    if pos < 0
+      if pos.abs > length
+        raise IndexError
+      end
+    else
+      if (pos + 1) > length
+        raise IndexError
       end
     end
+    
+    case start
+    when :bottom
+      @list[pos]
+    when :top, :peek
+      @list.reverse[pos]
+    else
+      raise ArgumentError
+    end
+  end
+  
+  # @param [#to_int] pos
+  def [](pos)
+    at pos
+  end
 
+  # @return [Array]
+  def to_a
+    @list.dup
+  end
+
+  # @return [self]
+  def freeze
+    @list.freeze
+    super
+  end
+  
+  # @return [String]
+  def inspect
+    "#<#{self.class} limit=#{@limit.inspect} #{@list.inspect}>"
+  end
+  
+  # @return [Boolean]
+  def ==(other)
+    (self.class == other.class) && @list == other._list
+  end
+  
+  def eql?(other)
+    (self.class == other.class) && 
+      [limit, @list].eql?([other.limit, other._list])
+  end
+
+  # @return [Integer]
+  def hash
+    [@limit, @list].hash
+  end
+  
+  protected
+  
+  def _list
+    @list
+  end
+
+  private
+
+  def initialize_copy(original)
+    @list = @list.dup
   end
 
 end
--- test/test_abstractstack.rb
+++ test/test_abstractstack.rb
@@ -3,14 +3,30 @@ require_relative 'test_helper'
 
 class TestAbstractStack < Test::Unit::TestCase
   
-  SimpleStack = AbstractStack.new
-
-  EnumStack   = AbstractStack.new do
+  class FIFO < AbstractStack
     include Enumerable
+    
+    alias_method :each, :fifo_each
+    alias_method :reverse_each, :filo_each
+  
+    def at(pos)
+      super pos, :bottom
+    end
   end
   
-  def test_normaly
-    stack = SimpleStack.new
+  class LIFO< AbstractStack
+    include Enumerable
+    
+    alias_method :each, :lifo_each
+    alias_method :reverse_each, :fifo_each
+  
+    def at(pos)
+      super pos, :top
+    end
+  end
+  
+  def test_fifo
+    stack = FIFO.new
 
     assert_equal nil, stack.limit
 
@@ -18,7 +34,7 @@ class TestAbstractStack < Test::Unit::TestCase
     assert_equal nil, stack.top
     assert_equal 0, stack.length
 
-    assert_raises SimpleStack::UnderFlow do
+    assert_raises FIFO::UnderFlow do
       stack.pop
     end
 
@@ -36,7 +52,43 @@ class TestAbstractStack < Test::Unit::TestCase
     assert_equal [1, 'String'], stack.to_a
     assert_equal 1, stack.bottom
     assert_equal 'String', stack.top
+    
+    assert_equal 1, stack[0]
+    assert_equal 1, stack[-2]
+    assert_equal 'String', stack[1]
+    assert_equal 'String', stack[-1]
+    
+    assert_raises IndexError do
+      stack[2]
+    end
+    
+    assert_raises IndexError do
+      stack[-3]
+    end
+    
+    assert_equal '#<TestAbstractStack::FIFO limit=nil [1, "String"]>', stack.inspect
+    
+    stack2 = FIFO.new
+    stack2 << 1
+    assert_same false, stack == stack2
+    assert_same false, stack.eql?(stack2)
+    stack2 << 'String'
+    assert_same true, stack == stack2
+    assert_same true, stack.eql?(stack2)
+    assert_same true, {stack => true}.has_key?(stack2)
 
+    stack3 = FIFO.new 3
+    stack3 << 1 << 'String'
+    assert_same true, stack == stack3
+    assert_same false, stack.eql?(stack3)
+    assert_same false, {stack => true}.has_key?(stack3)
+    
+    stack4 = stack3.dup
+    assert_equal 3, stack4.limit
+    assert_same true, stack3 == stack4
+    stack4 << :sth
+    assert_same false, stack3 == stack4
+    
     enum = stack.each
     assert_kind_of Enumerable, enum
     assert_equal 1, enum.next
@@ -44,6 +96,8 @@ class TestAbstractStack < Test::Unit::TestCase
     assert_raises StopIteration do
       enum.next
     end
+    
+    assert_same stack, stack.each{|v|v}
 
     enum = stack.lifo_each
     assert_kind_of Enumerable, enum
@@ -64,24 +118,51 @@ class TestAbstractStack < Test::Unit::TestCase
     assert_equal 0, stack.length
   end
 
-
   def test_sizelimit
-    stack = SimpleStack.new 2
+    stack = FIFO.new 2
     
     assert_equal 2, stack.limit
     stack << 4 << 7
     
-    assert_raises SimpleStack::OverFlow do
+    assert_raises FIFO::OverFlow do
       stack << 9
     end
 
     assert_equal 2, stack.length
   end
 
-  def test_abstract
-    stack = EnumStack.new
-    stack << 1 << 5 << 9
-    assert_equal [2, 10, 18], stack.map{|v|v * 2}
+  def test_lifo
+    stack = LIFO.new
+
+    stack << 1 << 'String'
+
+    assert_equal 'String', stack[0]
+    assert_equal 'String', stack[-2]
+    
+    assert_equal 1, stack[1]
+    assert_equal 1, stack[-1]
+
+    assert_raises IndexError do
+      stack[2]
+    end
+    
+    assert_raises IndexError do
+      stack[-3]
+    end
+    
+    enum = stack.each
+    assert_kind_of Enumerable, enum
+    assert_equal 'String', enum.next
+    assert_equal 1, enum.next
+    assert_raises StopIteration do
+      enum.next
+    end
+    
+    stack2 = LIFO.new
+    stack2 << 1 << 7
+    assert_equal [14, 2], stack2.map{|v|v * 2}
+    
+    assert_equal 7, stack2[0]
   end
 
 end
